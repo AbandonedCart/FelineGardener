ca19419747f1491f30c87aa364ddb4e63c25cfd1
TwistedUmbrellaX <twistedumbrella@gmail.com>
2026-04-18 19:35:59 -0400

    Add a proper icon and fix imports for update

--- app/src/main/java/com/felinegardener/toxicplants/MainActivity.kt
+++ app/src/main/java/com/felinegardener/toxicplants/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.ComponentName
 import android.content.Context
 import android.app.PendingIntent
+import android.content.pm.PackageInstaller
 import android.net.Uri
 import android.os.Bundle
 import android.os.Build
